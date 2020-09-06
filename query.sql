bbe24a79b4e67a0965fc59ff23a5cf21c8e3f1ec
[stefanovezzoni] <[stefanov.santos@gmail.com]>
2020-09-06 13:28:03 -0300

    Query deleted

--- query.sql
+++
@@ -1,7 +0,0 @@
-create table users (
-			users_id int not null auto_increment,
-            name VARCHAR(255) default null,
-			salary int(11) default null,
-            team_name VARCHAR(255) default null,
-            primary key(users_id)
-            ) ENGINE = INNODB;
